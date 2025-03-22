35c92c9bda5a92f4aa76398cebe3fdada54125a9
M0S4YSH3LL0 <moritz@buettner.info>
2025-03-22 09:19:51 +0100

    Moved #defines out of sysfetch.c
    - added .gitignore rule
    - added sysfetch.h

--- src/sysfetch.c
+++ src/sysfetch.c
@@ -1,3 +1,4 @@
+#include "sysfetch.h"
 #include <features.h>
 #include <stddef.h>
 #include <stdio.h>
@@ -6,21 +7,6 @@
 #include <sys/types.h>
 #include <sys/utsname.h>
 #include <unistd.h>
-
-#define VERSION "0.38"
-#define AUTHOR "m0s4ysh3ll0"
-
-#define BUFFER_SIZE 256
-
-// color codes
-#define NRM "\x1B[0m"
-#define RED "\x1B[31m"
-#define GRN "\x1B[32m"
-#define YEL "\x1B[33m"
-#define BLU "\x1B[34m"
-#define MAG "\x1B[35m"
-#define CYN "\x1B[36m"
-#define WHT "\x1B[37m"
 
 // Default labels
 char hostname_label[BUFFER_SIZE] = "host";
